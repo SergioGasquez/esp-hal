acf3d21bb91695ea659ef3d5cb6d2dee04e26caf
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-09-22 15:01:21 +0200

    feat: Initial DMA for SHA implementation

--- esp-hal-common/src/dma/mod.rs
+++ esp-hal-common/src/dma/mod.rs
@@ -240,6 +240,9 @@ impl DmaLinkedListDw0 for &mut u32 {
     }
 }
 
+/// Marks channels as useable for SHA
+pub trait ShaPeripheral: PeripheralMarker {}
+
 /// Marks channels as useable for SPI
 pub trait SpiPeripheral: PeripheralMarker {}
 
