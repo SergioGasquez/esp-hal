2a6590f95156e783d3dd1994d40cbed3771e93a8
Sergio Gasquez <sergio.gasquez@gmail.com>
2024-04-17 12:28:04 +0200

    feat: Allow 26/40 MHz targets

--- hil-test/tests/clock_monitor.rs
+++ hil-test/tests/clock_monitor.rs
@@ -11,17 +11,25 @@ use esp_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, rtc_cnt
 
 struct Context<'a> {
     rtc: Rtc<'a>,
+    #[cfg(any(feature = "esp32", feature = "esp32c2"))]
+    xtal_freq: u32,
 }
 
 impl Context<'_> {
     pub fn init() -> Self {
         let peripherals = Peripherals::take();
         let system = peripherals.SYSTEM.split();
+        #[cfg(any(feature = "esp32", feature = "esp32c2"))]
+        let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+        #[cfg(not(any(feature = "esp32", feature = "esp32c2")))]
         ClockControl::boot_defaults(system.clock_control).freeze();
-
         let rtc = Rtc::new(peripherals.LPWR, None);
 
-        Context { rtc }
+        Context {
+            rtc,
+            #[cfg(any(feature = "esp32", feature = "esp32c2"))]
+            xtal_freq: clocks.xtal_clock.to_MHz(),
+        }
     }
 }
 
@@ -37,11 +45,19 @@ mod tests {
 
     #[test]
     fn test_estimated_clock(mut ctx: Context<'static>) {
-        #[cfg(feature = "esp32c2")] // 26 MHz
-        defmt::assert!((23..=29).contains(&ctx.rtc.estimate_xtal_frequency()));
+        #[cfg(any(feature = "esp32", feature = "esp32c2"))] // 26 MHz
+        {
+            if ctx.xtal_freq == 26 {
+                // 26 MHz
+                defmt::assert!((23..=29).contains(&ctx.rtc.estimate_xtal_frequency()));
+            } else {
+                // 40 MHz
+                defmt::assert!((35..=45).contains(&ctx.rtc.estimate_xtal_frequency()));
+            }
+        }
         #[cfg(feature = "esp32h2")] // 32 MHz
         defmt::assert!((29..=35).contains(&ctx.rtc.estimate_xtal_frequency()));
-        #[cfg(not(any(feature = "esp32h2", feature = "esp32c2")))] // 40 MHz
+        #[cfg(not(any(feature = "esp32", feature = "esp32c2", feature = "esp32h2")))] // 40 MHz
         defmt::assert!((35..=45).contains(&ctx.rtc.estimate_xtal_frequency()));
     }
 }
